92926a89a4ec8c62815bd27b9bb6d998a1550a30
psolomin <p.o.solomin@gmail.com>
2022-01-08 23:33:50 +0000

    Add dummy project for KinesisIO

--- file-sink-with-window/src/test/java/MyTest.java
+++ file-sink-with-window/src/test/java/MyTest.java
@@ -1,4 +1,4 @@
-import com.pavel.example.MyConverters;
+import com.psolomin.example.MyConverters;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
---
+++ kinesis-io-with-enhanced-fan-out/src/main/java/com/psolomin/example/Main.java
@@ -0,0 +1,5 @@
+package com.psolomin.example;
+
+public class Main {
+    public static void main(String[] args) {}
+}
