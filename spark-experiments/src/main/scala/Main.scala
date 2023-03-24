122d23615dff5db05bbbfcb339442b7eacc1005f
psolomin <psolomin@users.noreply.github.com>
2023-03-24 16:57:15 +0000

    Spark experiments (#7)
    * Add code for inserting into a database
    * Upgrade spark testing base

---
+++ spark-experiments/src/main/scala/Main.scala
@@ -0,0 +1,33 @@
+import org.apache.spark.sql.functions.{col, udf}
+import org.apache.spark.sql.{DataFrame, SaveMode, SparkSession}
+
+object Main {
+  def generateDf(sparkSession: SparkSession, numRecords: Long): DataFrame = {
+    val generatorFunc = udf((id: Long) => f"user-name-${id}%09d")
+    sparkSession.range(0L, numRecords).select(
+      col("id"),
+      generatorFunc(col("id")).as("user_name")
+    )
+  }
+
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder()
+      .master("local[4]")  // 4 threads
+      .getOrCreate()
+
+    val numRecords = args(0).toLong
+
+    generateDf(spark, numRecords).write
+      .format("jdbc")
+      .option("driver", "com.mysql.cj.jdbc.Driver")
+      .option("url", "jdbc:mysql://127.0.0.1:3306/my_db?rewriteBatchedStatements=true")
+      .option("dbtable", "my_table")
+      .option("user", "my")
+      .option("password", "my")
+      .option("truncate", "true")
+      .option("numPartitions", "4")
+      .option("batchsize", "10000")
+      .mode(SaveMode.Overwrite)
+      .save()
+  }
+}
