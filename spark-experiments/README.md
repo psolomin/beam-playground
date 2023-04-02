34e43e7d9bc21eeb9e620299457044927846409f
psolomin <p.o.solomin@gmail.com>
2023-04-02 15:35:01 +0100

    WIP

--- spark-experiments/README.md
+++ spark-experiments/README.md
@@ -35,7 +35,35 @@ mysql --host=127.0.0.1 --port=3306 \
 
 Requires:
 - kubectl 1.22.2 or higher
-- minikube 1.22.0 or higher
+- minikube 1.29.0 or higher
 
-Follow the scripts under [k8s dir](./k8s).
+If you need to generate your own images and want to use Spark distro tooling:
 
+```
+mkdir -p distros
+
+curl https://dlcdn.apache.org/spark/spark-3.3.2/spark-3.3.2-bin-hadoop3.tgz \
+    -o ./distros/spark-3.3.2-bin-hadoop3.tgz
+
+tar zxvf ./distros/spark-3.3.2-bin-hadoop3.tgz --directory ./distros
+
+./distros/spark-3.3.2-bin-hadoop3/bin/docker-image-tool.sh \
+    -r my-spark.foo/my-spark -t 3.3.2-java11 build
+
+minikube image load my-spark.foo/my-spark/spark:3.3.2-java11 --overwrite=true
+
+```
+
+When image is loaded, follow the scripts under [k8s dir](./k8s).
+
+Once finished, you can clean up via:
+
+```
+minikube delete --all --purge
+```
+
+
+Caused by: javax.net.ssl.SSLPeerUnverifiedException: Hostname minikube not verified:
+certificate: sha256/+E7cgJM8Kc38pbSt3ot6BZl2R0qj8Qi668pNGq2s/Jo=
+DN: CN=minikube, O=system:masters
+subjectAltNames: [192.168.49.2, 10.96.0.1, 127.0.0.1, 10.0.0.1, minikubeCA, control-plane.minikube.internal, kubernetes.default.svc.cluster.local, kubernetes.default.svc, kubernetes.default, kubernetes, localhost]
--- spark-experiments/k8s/setup.sh
+++ spark-experiments/k8s/setup.sh
@@ -1,10 +1,37 @@
 #!/usr/bin/env bash
 
+# Adopted from https://timothyzhang.medium.com/running-spark-shell-on-kubernetes-3181d4446622
+
 # start cluster
 minikube config set cpus 4
-minikube config set memory 8Gi
-minikube start
+minikube config set memory 8G
+minikube start --insecure-registry "10.0.0.0/24"
+minikube addons enable ingress
+minikube addons enable ingress-dns
 
-# create spark namespace
+# create spark namespace:
 kubectl apply -f namespace.yaml
 
+# load local config files into mountable volumes:
+kubectl -n my-spark create configmap configs \
+  --from-file=exec_tpl=./executor-template.yaml
+
+# create client pod - apps will be submitted from it in local or cluster mode:
+kubectl apply -f spark-shell-pod.yaml
+
+kubectl -n my-spark exec -it spark-client -- sh -c 'cd /opt/spark/; ./bin/spark-shell \
+    --master k8s://https://kubernetes.default.svc:443 \
+    --deploy-mode client \
+    --conf spark.kubernetes.namespace=my-spark \
+    --conf spark.kubernetes.container.image=my-spark.foo/my-spark/spark:3.3.2-java11 \
+    --conf spark.kubernetes.container.image.pullPolicy=Never  \
+    --conf spark.kubernetes.authenticate.driver.serviceAccountName=my-spark-sa \
+    --conf spark.kubernetes.authenticate.serviceAccountName=my-spark-sa \
+    --conf spark.kubernetes.driver.pod.name=spark-client \
+    --conf spark.kubernetes.executor.podTemplateFile=file:///etc/spark/executor-template.yaml \
+    --conf spark.executor.instances=1 \
+    --conf spark.executor.memory=1G \
+    --conf spark.driver.memory=512M \
+    --conf spark.driver.host=spark-client-headless \
+    --conf spark.driver.port=19987 \
+    --conf spark.jars.ivy=/tmp/.ivy'
