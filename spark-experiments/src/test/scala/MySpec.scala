a87230b786f9cff3ef12c1868cadb8c614e112c6
psolomin <p.o.solomin@gmail.com>
2023-06-16 13:30:34 +0100

    Add test with nullified literal

--- spark-experiments/src/test/scala/MySpec.scala
+++ spark-experiments/src/test/scala/MySpec.scala
@@ -1,5 +1,6 @@
 import com.holdenkarau.spark.testing.DataFrameSuiteBase
 import org.apache.spark.sql.Row
+import org.apache.spark.sql.functions.{lit, when}
 import org.apache.spark.sql.types.{LongType, StringType, StructField, StructType}
 import org.scalatest.flatspec.AnyFlatSpec
 
@@ -16,5 +17,20 @@ class MySpec extends AnyFlatSpec with DataFrameSuiteBase {
       ))
     )
     assertDataFrameEquals(expected, df)
+  }
+
+  it should "be nullable" in {
+    val df = spark.createDataFrame(
+      spark.sparkContext.parallelize(List(Row(0L))),
+      StructType(Array(StructField("id", LongType, nullable = false)))
+    )
+    val schema = df.withColumn("c2", when(lit(true), lit("a"))).schema
+    assert(schema.fields(0).name, "id")
+    assert(schema.fields(1).name, "c2")
+    assert(!schema.fields(0).nullable)
+    assert(schema.fields(1).nullable)
+
+    val df2 = spark.sql("select case when true then 'abc' end as my_col")
+    assert(df2.schema.fields(0).nullable)
   }
 }
