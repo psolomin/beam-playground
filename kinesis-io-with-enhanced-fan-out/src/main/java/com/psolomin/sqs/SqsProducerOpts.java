3d271a35708b6aab2ca586e4bc0167b6b18a9fb9
psolomin <psolomin@users.noreply.github.com>
2023-09-27 22:37:49 +0100

    Add more experiments & examples (#15)

---
+++ kinesis-io-with-enhanced-fan-out/src/main/java/com/psolomin/sqs/SqsProducerOpts.java
@@ -0,0 +1,28 @@
+package com.psolomin.sqs;
+
+import org.apache.beam.runners.flink.FlinkPipelineOptions;
+import org.apache.beam.sdk.io.aws2.options.AwsOptions;
+import org.apache.beam.sdk.options.Default;
+import org.apache.beam.sdk.options.Description;
+import org.apache.beam.sdk.options.PipelineOptions;
+import org.apache.beam.sdk.options.Validation;
+
+public interface SqsProducerOpts extends PipelineOptions, FlinkPipelineOptions, AwsOptions {
+    @Validation.Required
+    @Description("Queue URL for writing.")
+    String getOutputQueueUrl();
+
+    void setOutputQueueUrl(String value);
+
+    @Validation.Required
+    @Description("Total N of messages to write.")
+    Integer getMsgsToWrite();
+
+    void setMsgsToWrite(Integer value);
+
+    @Default.Boolean(false)
+    @Description("Enable or disable batched writes. Disabled by default.")
+    Boolean getEnableBatchedWrites();
+
+    void setEnableBatchedWrites(Boolean value);
+}
